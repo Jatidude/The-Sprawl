bd80fa8c0e88c6b38678c345cc6d82107b729564
Jacob Timme <jacob.timme@gmail.com>
2015-01-10 09:54:21 -0800

    Adding more ailments

--- src/Ailment.java
+++ src/Ailment.java
@@ -1,4 +1,38 @@
 public enum Ailment {
-	HEALTHY
-
+	HEALTHY{
+		String name = "no diseases";
+		public String getName(){
+			return this.name;
+		}
+	},
+	FLU{
+		String name = "the Flu";
+		public String getName(){
+			return this.name;
+		}
+	},
+	DYSENTERY{
+		String name = "the bloody Flux";
+		public String getName(){
+			return this.name;
+		}
+	},
+	MALARIA{
+		String name = "teh Ague";
+		public String getName(){
+			return this.name;
+		}
+	},
+	PLAGUE{
+		String name = "the Black PLague";
+		public String getName(){
+			return this.name;
+		}
+	},
+	SMALLPOX{
+		String name = "the Red Plague";
+		public String getName(){
+			return this.name;
+		}
+	}
 }
