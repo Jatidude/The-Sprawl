b17d589e8c37edfbc75cbc097e86f876202375a1
Nick Hemann <NickHemann@gmail.com>
2015-01-10 20:01:40 -0500

    Update 0.010
    Minor changes.

--- src/Game.java
+++ src/Game.java
@@ -7,14 +7,7 @@
 public class Game {
 	
 	public static void main(String[] args) {
-		Colony a = new Colony("The Outskirts", 12);
-		Colony b = new Colony("The Underground", 12);
-		Caravan c = new Caravan(a.colonists.get(0));
-		System.out.println(c.caravansary.get(0).getName());
-		System.out.println(c.caravansary.get(0).getAge());
-		System.out.println(c.caravansary.get(0).getJob());
-		c.embark(b);
-		System.out.println(c.getStatus());
+		System.out.println("Hello, World!");
 	}
-	
+
 }
